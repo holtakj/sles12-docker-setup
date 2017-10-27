2c296b6dc52b8ae03f9b57eb0e2f519d5d357866
Juraj Holtak <juraj.holtak@ucs.at>
2017-10-27 11:27:55 +0200

    prepare script might be ready

--- sles12sp3/prepare.sh
+++ sles12sp3/prepare.sh
@@ -5,12 +5,22 @@
 echo "Preparing environment for SLES12 SP3 Docker machine..."
 
 echo "Adding repositories..."
-echo "Answer with 'a' to trust the repos"
+
 zypper addrepo -fc http://download.opensuse.org/repositories/openSUSE:/Backports:/SLE-12/standard/openSUSE:Backports:SLE-12.repo
 zypper addrepo -fc http://download.opensuse.org/repositories/openSUSE:/Backports:/SLE-12-SP1/standard/openSUSE:Backports:SLE-12-SP1.repo
 zypper addrepo -fc http://download.opensuse.org/repositories/openSUSE:/Backports:/SLE-12-SP2/standard/openSUSE:Backports:SLE-12-SP2.repo
 zypper addrepo -fc http://download.opensuse.org/repositories/openSUSE:/Backports:/SLE-12-SP3/standard/openSUSE:Backports:SLE-12-SP3.repo
+
+
+echo "Please answer with 'a' to trust all repos..."
+sleep 3
+
 zypper refresh
+
+zypper install ansible
+
+
+
 
 
 
