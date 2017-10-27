582e9c1e9c4e4c94065c2d6186c97d66a064ecb0
Juraj Holtak <juraj.holtak@ucs.at>
2017-10-27 11:00:57 +0200

    add backports repos

--- sles12sp3/prepare.sh
+++ sles12sp3/prepare.sh
@@ -5,6 +5,11 @@
 echo "Preparing environment for SLES12 SP3 Docker machine..."
 
 echo "Adding repositories..."
+zypper -n addrepo -fc http://download.opensuse.org/repositories/openSUSE:/Backports:/SLE-12/standard/openSUSE:Backports:SLE-12.repo | echo 'a'
+zypper -n addrepo -fc http://download.opensuse.org/repositories/openSUSE:/Backports:/SLE-12-SP1/standard/openSUSE:Backports:SLE-12-SP1.repo | echo 'a'
+zypper -n addrepo -fc http://download.opensuse.org/repositories/openSUSE:/Backports:/SLE-12-SP2/standard/openSUSE:Backports:SLE-12-SP2.repo | echo 'a'
+zypper -n addrepo -fc http://download.opensuse.org/repositories/openSUSE:/Backports:/SLE-12-SP3/standard/openSUSE:Backports:SLE-12-SP3.repo | echo 'a'
+zypper -n refresh
 
 
 
