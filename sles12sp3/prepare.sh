05f91859f0de53afbbfde8c49a4a9ba5dcc3ff88
Juraj Holtak <juraj.holtak@ucs.at>
2017-10-27 19:05:39 +0200

    suse is sooo bad

--- sles12sp3/prepare.sh
+++ sles12sp3/prepare.sh
@@ -6,12 +6,11 @@ echo "Preparing environment for SLES12 SP3 Docker machine..."
 
 echo "Adding repositories..."
 
-#zypper addrepo -fc http://download.opensuse.org/repositories/openSUSE:/Backports:/SLE-12/standard/openSUSE:Backports:SLE-12.repo
-#zypper addrepo -fc http://download.opensuse.org/repositories/openSUSE:/Backports:/SLE-12-SP1/standard/openSUSE:Backports:SLE-12-SP1.repo
-#zypper addrepo -fc http://download.opensuse.org/repositories/openSUSE:/Backports:/SLE-12-SP2/standard/openSUSE:Backports:SLE-12-SP2.repo
-#zypper addrepo -fc http://download.opensuse.org/repositories/openSUSE:/Backports:/SLE-12-SP3/standard/openSUSE:Backports:SLE-12-SP3.repo
+zypper addrepo -fc http://download.opensuse.org/repositories/openSUSE:/Backports:/SLE-12/standard/openSUSE:Backports:SLE-12.repo
+zypper addrepo -fc http://download.opensuse.org/repositories/openSUSE:/Backports:/SLE-12-SP1/standard/openSUSE:Backports:SLE-12-SP1.repo
+zypper addrepo -fc http://download.opensuse.org/repositories/openSUSE:/Backports:/SLE-12-SP2/standard/openSUSE:Backports:SLE-12-SP2.repo
+zypper addrepo -fc http://download.opensuse.org/repositories/openSUSE:/Backports:/SLE-12-SP3/standard/openSUSE:Backports:SLE-12-SP3.repo
 #zypper addrepo http://download.opensuse.org/repositories/devel:languages:python/SLE_12_SP3/devel:languages:python.repo
-
 zypper addrepo -fc http://download.opensuse.org/repositories/Virtualization:/containers/openSUSE_Leap_42.1/Virtualization:containers.repo
 
 echo "Please answer with 'a' to trust all repos..."
