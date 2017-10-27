a38ccfd27fc47860b5697ceadc54bebec2d9d234
Juraj Holtak <juraj.holtak@ucs.at>
2017-10-27 11:17:03 +0200

    fix a bug

--- sles12sp3/prepare.sh
+++ sles12sp3/prepare.sh
@@ -5,10 +5,10 @@
 echo "Preparing environment for SLES12 SP3 Docker machine..."
 
 echo "Adding repositories..."
-zypper -n addrepo -fc http://download.opensuse.org/repositories/openSUSE:/Backports:/SLE-12/standard/openSUSE:Backports:SLE-12.repo | echo 'a'
-zypper -n addrepo -fc http://download.opensuse.org/repositories/openSUSE:/Backports:/SLE-12-SP1/standard/openSUSE:Backports:SLE-12-SP1.repo | echo 'a'
-zypper -n addrepo -fc http://download.opensuse.org/repositories/openSUSE:/Backports:/SLE-12-SP2/standard/openSUSE:Backports:SLE-12-SP2.repo | echo 'a'
-zypper -n addrepo -fc http://download.opensuse.org/repositories/openSUSE:/Backports:/SLE-12-SP3/standard/openSUSE:Backports:SLE-12-SP3.repo | echo 'a'
+zypper addrepo -fc http://download.opensuse.org/repositories/openSUSE:/Backports:/SLE-12/standard/openSUSE:Backports:SLE-12.repo | echo 'a'
+zypper addrepo -fc http://download.opensuse.org/repositories/openSUSE:/Backports:/SLE-12-SP1/standard/openSUSE:Backports:SLE-12-SP1.repo | echo 'a'
+zypper addrepo -fc http://download.opensuse.org/repositories/openSUSE:/Backports:/SLE-12-SP2/standard/openSUSE:Backports:SLE-12-SP2.repo | echo 'a'
+zypper addrepo -fc http://download.opensuse.org/repositories/openSUSE:/Backports:/SLE-12-SP3/standard/openSUSE:Backports:SLE-12-SP3.repo | echo 'a'
 zypper -n refresh
 
 
