e3c0703acda7fbcb0d5f4d32aef42b865265ae05
Juraj Holtak <juraj.holtak@ucs.at>
2017-10-27 11:25:20 +0200

    zypper sucks

--- sles12sp3/prepare.sh
+++ sles12sp3/prepare.sh
@@ -5,11 +5,12 @@
 echo "Preparing environment for SLES12 SP3 Docker machine..."
 
 echo "Adding repositories..."
-zypper addrepo -fc http://download.opensuse.org/repositories/openSUSE:/Backports:/SLE-12/standard/openSUSE:Backports:SLE-12.repo | echo 'a'
-zypper addrepo -fc http://download.opensuse.org/repositories/openSUSE:/Backports:/SLE-12-SP1/standard/openSUSE:Backports:SLE-12-SP1.repo | echo 'a'
-zypper addrepo -fc http://download.opensuse.org/repositories/openSUSE:/Backports:/SLE-12-SP2/standard/openSUSE:Backports:SLE-12-SP2.repo | echo 'a'
-zypper addrepo -fc http://download.opensuse.org/repositories/openSUSE:/Backports:/SLE-12-SP3/standard/openSUSE:Backports:SLE-12-SP3.repo | echo 'a'
-zypper -n refresh
+echo "Answer with 'a' to trust the repos"
+zypper addrepo -fc http://download.opensuse.org/repositories/openSUSE:/Backports:/SLE-12/standard/openSUSE:Backports:SLE-12.repo
+zypper addrepo -fc http://download.opensuse.org/repositories/openSUSE:/Backports:/SLE-12-SP1/standard/openSUSE:Backports:SLE-12-SP1.repo
+zypper addrepo -fc http://download.opensuse.org/repositories/openSUSE:/Backports:/SLE-12-SP2/standard/openSUSE:Backports:SLE-12-SP2.repo
+zypper addrepo -fc http://download.opensuse.org/repositories/openSUSE:/Backports:/SLE-12-SP3/standard/openSUSE:Backports:SLE-12-SP3.repo
+zypper refresh
 
 
 
