40a6de3376fc53cf56f52fca5898e0b4cdbb50b1
Juraj Holtak <juraj.holtak@ucs.at>
2017-10-29 16:38:43 +0100

    hmmm

--- sles12sp3/jenkins-server.sh
+++ sles12sp3/jenkins-server.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-DIR/docker-host.sh
-ansible-playbook -v $DIR/ansible/docker-host.yaml
+$DIR/docker-host.sh
+
+ansible-playbook -v $DIR/ansible/jenkins-server.yaml
