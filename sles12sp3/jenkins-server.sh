419c8567a8999b64aaeb07a3ce680147619abb77
Juraj Holtak <juraj.holtak@ucs.at>
2017-10-29 17:46:53 +0100

    jenkins

--- sles12sp3/jenkins-server.sh
+++ sles12sp3/jenkins-server.sh
@@ -3,4 +3,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 $DIR/docker-host.sh
 
-docker-compose -f $DIR/../common/docker-compose/jenkins/docker-compose.yaml
+/usr/local/bin/docker-compose -f $DIR/../common/docker-compose/jenkins/docker-compose.yaml
