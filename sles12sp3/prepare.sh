31999bdf5c9c735926f8ab7ef8446b0a48adc5c0
Juraj Holtak <juraj.holtak@ucs.at>
2017-10-29 19:46:50 +0100

    jenkins

--- sles12sp3/prepare.sh
+++ sles12sp3/prepare.sh
@@ -2,9 +2,9 @@
 
 . ../common/enforce_root.sh
 
-echo "Preparing environment for SLES12 SP3 Docker machine..."
+echo "Preparing environment for SLES12 SP3 Docker machine..." &&
 
-echo "Adding repositories..."
+echo "Adding repositories..." &&
 
 zypper addrepo -fc http://download.opensuse.org/repositories/openSUSE:/Backports:/SLE-12/standard/openSUSE:Backports:SLE-12.repo &&
 zypper addrepo -fc http://download.opensuse.org/repositories/openSUSE:/Backports:/SLE-12-SP1/standard/openSUSE:Backports:SLE-12-SP1.repo &&
@@ -15,19 +15,19 @@ zypper addrepo http://download.opensuse.org/repositories/devel:languages:python/
 #zypper addrepo -fc http://download.opensuse.org/repositories/devel:/languages:/python/openSUSE_Leap_42.3/devel:languages:python.repo
 
 
-echo "Please answer with 'a' to trust all repos..."
-sleep 3
+echo "Please answer with 'a' to trust all repos..." &&
+sleep 3 &&
 
 zypper refresh &&
 
-echo "SUSE zypper tool sucks and thats why its going to present you with a nice conflict."
-echo "Do not be scared and take first resolution possible and proceed with the install."
-sleep 5
+echo "SUSE zypper tool sucks and thats why its going to present you with a nice conflict." &&
+echo "Do not be scared and take first resolution possible and proceed with the install." &&
+sleep 5 &&
 
 zypper install ansible &&
 
-echo  -n "Installed ";
-ansible-playbook --version &&
+echo  -n "Installed " &&
+ansible-playbook --version
 
 
 
