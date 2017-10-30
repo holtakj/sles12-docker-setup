aa299d12268cccb2b6daca27939661849db44bf3
Juraj Holtak <juraj.holtak@ucs.at>
2017-10-30 11:22:58 +0100

    better dir handling

--- update.sh
+++ update.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+. $DIR/common/enforce_root.sh
 
-. common/enforce_root.sh
+ACTDIR=`pwd`
 
 echo "Updating..."
+cd $DIR
 git pull
+cd $ACTDIR
