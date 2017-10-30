4308cf44c1fe1a55e7d7ef3bb4a9512bb885a501
Juraj Holtak <juraj.holtak@ucs.at>
2017-10-30 14:37:44 +0100

    cu

--- sles12sp3/jenkins-server.sh
+++ sles12sp3/jenkins-server.sh
@@ -3,9 +3,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 . $DIR/../common/enforce_root.sh
 
-
-$DIR/docker-host.sh
-
 ansible-playbook $DIR/../common/ansible/jenkins_server.yaml &&
 
 /usr/local/bin/docker-compose -f $DIR/../common/docker-compose/jenkins/docker-compose.yaml up
