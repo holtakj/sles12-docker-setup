8639759ce9e25bda31012b8b1d5db2cb500cd4c3
Juraj Holtak <juraj.holtak@ucs.at>
2017-10-30 10:09:55 +0100

    add virtualization repo to docker install

--- sles12sp3/docker-host.sh
+++ sles12sp3/docker-host.sh
@@ -3,5 +3,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 . $DIR/../common/enforce_root.sh
 
+zypper addrepo -fc http://download.opensuse.org/repositories/Virtualization:/containers/SLE_12_SP3/Virtualization:containers.repo &&
+echo "Please answer with 'a' to trust all repos..." &&
+sleep 3
+
+zypper refresh &&
 
 ansible-playbook $DIR/../common/ansible/install_docker.yaml
