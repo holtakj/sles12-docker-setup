37d842feb3a85ef93c7067a50b6fdd4eaca91dfc
Juraj Holtak <juraj.holtak@ucs.at>
2017-10-30 14:44:47 +0100

    cu

--- sles12sp3/clearing-unit.sh
+++ sles12sp3/clearing-unit.sh
@@ -3,6 +3,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 . $DIR/../common/enforce_root.sh
 
-ansible-playbook $DIR/../common/ansible/clearing-unit.yaml 
+ansible-playbook $DIR/../common/ansible/clearing-unit.yaml
 
 #/usr/local/bin/docker-compose -f $DIR/../common/docker-compose/jenkins/docker-compose.yaml up
