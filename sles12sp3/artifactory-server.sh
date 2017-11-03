efffa03dde4dfde628eb35becb6b7803663f9e21
Juraj Holtak <juraj.holtak@ucs.at>
2017-11-03 10:18:03 +0100

    artifactory first hit

---
+++ sles12sp3/artifactory-server.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+. $DIR/../common/enforce_root.sh
+
+ansible-playbook $DIR/../common/ansible/artifactory_server.yaml &&
+
+/usr/local/bin/docker-compose -f $DIR/../common/docker-compose/artifactory/docker-compose.yaml up
