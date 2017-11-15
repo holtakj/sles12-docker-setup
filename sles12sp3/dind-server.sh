35fdc9c5c7d11f30c3e38d343c46b72a5c97f283
Juraj Holtak <juraj.holtak@ucs.at>
2017-11-15 15:47:04 +0100

    bugfixes

--- sles12sp3/dind-server.sh
+++ sles12sp3/dind-server.sh
@@ -3,6 +3,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 . $DIR/../common/enforce_root.sh
 
-ansible-playbook $DIR/../common/ansible/dind.yaml
+ansible-playbook $DIR/../common/ansible/dind.yaml &&
 
 /usr/local/bin/docker-compose -f $DIR/../common/docker-compose/dind/docker-compose.yaml up
